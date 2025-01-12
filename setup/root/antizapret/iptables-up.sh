461eaccf3eaee360c73e6e8f2a6deaf52171298d
busy10 <59513719+busy10@users.noreply.github.com>
2025-01-12 13:26:17 +0300

    1

--- setup/root/antizapret/iptables-up.sh
+++ setup/root/antizapret/iptables-up.sh
@@ -79,6 +79,7 @@ iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 ! -d 10.29.0.1/32 -p udp --dpor
 iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 ! -d 10.30.0.0/15 -j CONNMARK --set-xmark 0x1/0xffffffff
 # ANTIZAPRET-MAPPING
 iptables -w -t nat -N ANTIZAPRET-MAPPING
+iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 -d 10.28.0.0/15 -j ANTIZAPRET-MAPPING
 iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 -d 10.30.0.0/15 -j ANTIZAPRET-MAPPING
 # MASQUERADE
 iptables -w -t nat -A POSTROUTING -s 10.28.0.0/15 -j MASQUERADE
